2785556cef7981efb818208554212aef05d985b3
issueyh <issueyh12921@gmail.com>
2021-06-01 23:21:12 +0800

    feat: add delete bistro function

--- app.js
+++ app.js
@@ -81,6 +81,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
         .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+    const id = req.params.id
+    return Bistro.findById(id)
+        .then(restaurant => restaurant.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
+
 app.get('/search', (req, res) => {
     const keyword = req.query.keyword
     const restaurants = restaurantList.results.filter(restaurant => {
